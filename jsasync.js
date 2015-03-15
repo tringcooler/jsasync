1ea743517aa9bf34146504aaa665176dbef8b350
tringcooler <tringcooler@users.noreply.github.com>
2015-03-16 07:35:49 +0800

    Update jsasync.js

--- jsasync.js
+++ jsasync.js
@@ -25,8 +25,9 @@ var jsasync = (function() {
 			if(seq_idx[1].length < 2) return null;
 			seq_idx[1].shift();
 			seq_idx[1][0] ++;
+			bra = -1;
 		} else if(itm instanceof Array) {
-			if(bra > 0) {
+			if(bra != 0) {
 				seq_idx[1][0] ++;
 				bra --;
 			} else {
@@ -43,11 +44,12 @@ var jsasync = (function() {
 		else seq_idx[1][0] ++;
 		return this._seq_cur(seq_idx, bra);
 	};
-	jsasync.prototype._run_hndl = function(si, tar, tar_idx, scp, varg, bra, args) {
+	jsasync.prototype.MAX_RETRY = 5;
+	jsasync.prototype._run_hndl = function(si, tar, tar_idx, scp, retry, varg, bra, args) {
 		var itm = this._seq_itm(si);
 		var hndl = {};
 		if(varg) {
-			args = Array.prototype.slice.call(arguments, 6);
+			args = Array.prototype.slice.call(arguments, 7);
 		}
 		if(typeof(itm) == 'function') {
 			hndl.var = function(k, v) {
@@ -56,6 +58,8 @@ var jsasync = (function() {
 				else
 					scp[k] = v;
 			};
+			if(retry >= this.MAX_RETRY) throw 'Retry out of time.'
+			hndl.retry = this._run_hndl.bind(this, si, tar, tar_idx.slice(), scp, retry + 1, false, bra, args);
 			if(tar.length > 0) {
 				var tar_itm, idx, len;
 				hndl.each = [];
@@ -85,24 +89,24 @@ var jsasync = (function() {
 					ti ++;
 				}
 				if(bra > 0) {
-					return this._run_hndl_next(si, [], [], scp, false, 0, args);
+					return this._run_hndl_next(si, [], [], scp, 0, false, 0, args);
 				}
 				if(ti >= tar.length) bra = 1;
-				hndl.break = this._run_hndl.bind(this, si, tar, tar_idx, scp, true, 1);
-				hndl.continue = this._run_hndl.bind(this, si, tar, tar_idx, scp, true, bra);
+				hndl.break = this._run_hndl.bind(this, si, tar, tar_idx, scp, 0, true, 1);
+				hndl.continue = this._run_hndl.bind(this, si, tar, tar_idx, scp, 0, true, bra);
 				return itm.apply(this.self, [hndl].concat(args));
 			} else {
-				hndl.return = this._run_hndl_next.bind(this, si, [], [], scp, true, 0);
-				hndl.branch = this._run_hndl_next.bind(this, si, [], [], scp, true);
+				hndl.return = this._run_hndl_next.bind(this, si, [], [], scp, 0, true, 0);
+				hndl.branch = this._run_hndl_next.bind(this, si, [], [], scp, 0, true);
 				return itm.apply(this.self, [hndl].concat(args));
 			}
 		} else {
 			tar.push(itm);
 			tar_idx.push(0);
-			return this._run_hndl_next(si, tar, tar_idx, scp, false, 0, args);
+			return this._run_hndl_next(si, tar, tar_idx, scp, 0, false, 0, args);
 		}
 	};
-	jsasync.prototype._run_hndl_next = function(si, tar, tar_idx, scp, varg, bra, args) {
+	jsasync.prototype._run_hndl_next = function(si, tar, tar_idx, scp, retry, varg, bra, args) {
 		si = this._seq_next(si, bra);
 		if(si == null) return;
 		arguments[0] = si
@@ -110,7 +114,7 @@ var jsasync = (function() {
 	};
 	jsasync.prototype.run = function(seq, args) {
 		var si = [seq, null];
-		return this._run_hndl_next(si, [], [], {}, false, 0, args);
+		return this._run_hndl_next(si, [], [], {}, 0, false, 0, args);
 	};
 	jsasync.prototype.foreach = function() {
 	};
@@ -119,7 +123,7 @@ var jsasync = (function() {
 
 function f1(hndl) { hndl.var('t1', [1,2,3,4]); hndl.var('flag', true); console.log('f1', arguments); hndl.return(1, 2, 3) }
 function f2(hndl) { hndl.var('t2', {a:123, b:456}); console.log('f2', arguments); if(hndl.var('flag')) hndl.branch(1, 777); hndl.return(888) }
-function f3(hndl) { console.log('f3', arguments); hndl.return(666) }
+function f3(hndl) { console.log('f3', arguments); if(!hndl.var('flag')){hndl.var('flag', true);hndl.retry()};hndl.return(666) }
 function f4(hndl) { console.log('f4', arguments, hndl.each); hndl.continue(4, 5) }
 function f5(hndl) { console.log('f5', arguments); hndl.return() }
 seq = [f1, f2, [f3], ['t1', 't2', f4], f5];
